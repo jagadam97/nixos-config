5adc7339d0344b0fe41a1adcfb18378de2b7b5f0
jagadam97 <dineshjagadam@gmail.com>
2026-02-15 12:37:15 +0530

    fix folding at home

--- modules/services/foldingathome.nix
+++ modules/services/foldingathome.nix
@@ -11,6 +11,9 @@
     extraArgs = [
       "--cpus=8"
       "--passkey=c1cf1dde94381870c1cf1dde94381870"
+      "--allow" "0.0.0.0/0"
+      "--http-addresses" "0.0.0.0:7396"
+      "--allowed-origins" "*"
     ];
   };
 }
