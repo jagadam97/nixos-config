67758811f2f2989301528b304e762a37d53d511a
jagadam97 <dineshjagadam@gmail.com>
2026-02-15 11:20:25 +0530

    add homemanager backup

--- flake.nix
+++ flake.nix
@@ -47,6 +47,7 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "bak";
               home-manager.users.dj = import ./home/users/dj;
             }
           ];
