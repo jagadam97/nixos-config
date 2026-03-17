5951dc0a72cd08649f4752eb20ce1a3099e43b08
Dinesh Jagadam <dinesh.reddy@juspay.in>
2026-03-18 02:21:36 +0530

    feat: add disko disk layout for kayda, wire up nixos-anywhere
    - Add hosts/kayda/disko.nix (GPT + 512MB EFI + ext4 root on nvme0n1)
    - Add disko flake input
    - Wire disko.nixosModules.disko into kayda nixosConfiguration
    - Fix network interface eth0 -> enp3s0 (confirmed via ip a)
    - Update hardware.nix: remove TODO fileSystems (disko manages them)
    - Update default.nix: import disko.nix

---
+++ hosts/kayda/disko.nix
@@ -0,0 +1,37 @@
+# Disk layout for kayda - nvme0n1 (465.8GB)
+# GPT + EFI + ext4 root
+{ ... }:
+
+{
+  disko.devices = {
+    disk = {
+      nvme = {
+        type = "disk";
+        device = "/dev/nvme0n1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "fmask=0077" "dmask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
